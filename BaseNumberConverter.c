aa1247bcc08163607e35ae358e92309624bd935a
Najmi <57437663+mnajmikl@users.noreply.github.com>
2022-01-21 22:50:50 +0800

    Reworked the helper function
    Merged the helper functions into basenumber()

--- BaseNumberConverter.c
+++ BaseNumberConverter.c
@@ -9,49 +9,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-unsigned int getnumberlength(unsigned int, unsigned int);
-const char* getbasenumber(unsigned int, unsigned int, 
-								unsigned int, const char*);
+const char* basenumber(unsigned int, unsigned int, const char*);
 const char* binary(unsigned int);
 const char* oct(unsigned int);
 const char* hex(unsigned int);
 
 int main(int argc, char** argv)
 {
-	printf_s("255 = bin(0x%s)\n", binary(255));
-	printf_s("255 = oct(0x%s)\n", oct(255));
-	printf_s("255 = hex(0x%s)", hex(255));
+	printf_s("1 = bin(0x%s)\n", binary(1));
+	printf_s("1 = oct(0x%s)\n", oct(1));
+	printf_s("1 = hex(0x%s)", hex(1));
 	return 0;
 }
 
 /*!
- @function getnumberlength
- @param decimal The decimal number to be converted
- @param base The base of the number
- @result A binary number of the given decimal number
+ @function basenumber
+ @param decimal: The decimal number to be converted
+ @param base: The base of the number
+ @param length: The length  of the base number
+ @param numbers: The numbers in the base number given
+ @result A number in the specified base of the given decimal number
 */
-unsigned int getnumberlength(unsigned int decimal, unsigned int base)
+const char* basenumber(unsigned int decimal, unsigned int base, const char* numbers)
 {
 	unsigned int length = 0;
+	unsigned int decimal_copy = decimal;
+
 	while (decimal > 0)
 	{
 		decimal = decimal / base;
 		length++;
 	}
-	return length;
-}
 
-/*!
- @function getbasenumber
- @param decimal The decimal number to be converted
- @param base The base of the number
- @param length The length  of the base number
- @param numbers The numbers in the base number given
- @result A binary number of the given decimal number
-*/
-const char* getbasenumber(unsigned int decimal, unsigned int base,
-							unsigned int length, const char* numbers)
-{
 	char* outnumber = (char*)calloc(length, sizeof(char));
 
 	if (outnumber == NULL)
@@ -60,13 +49,13 @@ const char* getbasenumber(unsigned int decimal, unsigned int base,
 		return '\0';
 	}
 
-	int length_copy = length - 1;
+	length--;
 
-	while (decimal > 0)
+	while (decimal_copy > 0)
 	{
-		*(outnumber + length_copy) = numbers[decimal % base];
-		decimal = decimal / base;
-		length_copy--;
+		*(outnumber + length) = numbers[decimal_copy % base];
+		decimal_copy = decimal_copy / base;
+		length--;
 	}
 
 	return outnumber;
@@ -74,33 +63,30 @@ const char* getbasenumber(unsigned int decimal, unsigned int base,
 
 /*!
  @function binary
- @param decimal The decimal number to be converted
+ @param decimal: The decimal number to be converted
  @result A binary number of the given decimal number
 */
 const char* binary(unsigned int decimal)
 {
-	unsigned int length = getnumberlength(decimal, 2);
-	return getbasenumber(decimal, 2, length, "01");
+	return basenumber(decimal, 2, "01");
 }
 
 /*!
  @function oct
- @param decimal The decimal number to be converted
+ @param decimal: The decimal number to be converted
  @result A octal number of the given decimal number
 */
 const char* oct(unsigned int decimal)
 {
-	unsigned int length = getnumberlength(decimal, 8);
-	return getbasenumber(decimal, 8, length, "01234567");
+	return basenumber(decimal, 8, "01234567");
 }
 
 /*!
  @function hex
- @param decimal The decimal number to be converted
+ @param decimal: The decimal number to be converted
  @result A hexadecimal number of the given decimal number
 */
 const char* hex(unsigned int decimal)
 {
-	unsigned int length = getnumberlength(decimal, 16);
-	return getbasenumber(decimal, 16, length, "0123456789ABCDEF");
+	return basenumber(decimal, 16, "0123456789ABCDEF");
 }
