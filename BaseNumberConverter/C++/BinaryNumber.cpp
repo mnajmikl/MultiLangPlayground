d1ed7192eafcfbd0c74a77d0299e4b15d8550893
Najmi <57437663+mnajmikl@users.noreply.github.com>
2022-09-23 23:29:44 +0800

    Modified code to use C++ container
    Modified code to use C++ container std::vector.

--- BaseNumberConverter/C++/BinaryNumber.cpp
+++ BaseNumberConverter/C++/BinaryNumber.cpp
@@ -1,13 +1,14 @@
 ///
 // BinaryNumber.cpp
 //
-// Added C++ 20 new methods like std::span() to iterate dynamically created arrays
+// This program demonstrates the use of std::vector and its reverse iteration
+// to create binary number string
 //
 ///
 
 #include <iostream>
 #include <string>
-#include <span>
+#include <vector>
 
 ///
 //  Function std::string getbinarynumber(unsigned int)
@@ -15,42 +16,27 @@
 ///
 std::string getbinarynumber(unsigned int decimal)
 {
-    int length = 0;
-    int copy = decimal;
-    ///
     // Find the length of the array to store the "binary"
     // numbers (remainders from division by 2)
-    ///
-    while (copy > 0)
-    {
-        copy = copy / 2;
-        length += 1;
-    }
-    // Dynamic integer array that stores the "binary" numbers
-    int *binnumber = new int[length]();
-    std::fill_n(binnumber, length, 0);
-    int length_copy = length - 1;
-    ///
     // 1. Store the remainder of decimal division by 2
     // 2. Divide the decimal using integer division by 2
     // The result is the reversed version of the binary number
-    ///
-    while (decimal > 0)
-    {
-        binnumber[length_copy] = decimal % 2;
-        decimal = decimal / 2;
-        length_copy--;
-    }
+	std::vector<int> binnumber;
+	while (decimal > 0)
+	{
+		binnumber.push_back(decimal % 2);
+		decimal = decimal / 2;
+	}
     std::string binstr;
-    for (int i : std::span(binnumber, length))
-        binstr += std::to_string(i);
-    delete[] binnumber;
+	for (auto i = binnumber.rbegin(); i != binnumber.rend(); ++i)
+        binstr += std::to_string(*i);
     return binstr;
 }
 
 int main()
 {
     std::cout<<"132 in binary is: " << getbinarynumber(132);
+	std::cout<<"\n69 in binary is: " << getbinarynumber(69);
     return 0;
 }
 
